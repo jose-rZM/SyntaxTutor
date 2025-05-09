97bde78257ebf6c5a6c4e7a7b78957c48528ece9
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2025-05-09 10:06:08 +0200

    shake user input when response is wrong

--- slrtutorwindow.h
+++ slrtutorwindow.h
@@ -45,6 +45,7 @@ public:
     void updateProgressPanel();
     void addUserState(unsigned id);
     void wrongAnimation();
+    void wrongUserResponseAnimation();
 
     // ------------------------------------------------------
     // ------------------------------------------------------
