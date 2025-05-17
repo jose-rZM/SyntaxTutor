854d35bfd11aa0a843b083b80f6cc09cddd59e1c
jose_r <100773386+jose-rZM@users.noreply.github.com>
2025-05-17 10:09:59 +0200

    fonts

--- main.cpp
+++ main.cpp
@@ -4,22 +4,17 @@
 #include <QFontDatabase>
 #include <QImageReader>
 
-QString loadFonts()
+void loadFonts()
 {
     int id1 = QFontDatabase::addApplicationFont(":/resources/NotoSans-Regular.ttf");
     int id2 = QFontDatabase::addApplicationFont(":/resources/NotoSans-Italic.ttf");
     int id3 = QFontDatabase::addApplicationFont(":/resources/NotoSans-Bold.ttf");
-
-    if (id1 == -1 || id2 == -1 || id3 == -1)
-        return "";
-
-    QString family = QFontDatabase::applicationFontFamilies(id1).at(0);
-    return family;
 }
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+<<<<<<< Updated upstream
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QString fontFamily = loadFonts();
     if (!fontFamily.isEmpty()) {
@@ -29,6 +24,10 @@ int main(int argc, char *argv[])
         QApplication::setFont(defaultFont);
 
     }
+=======
+
+    loadFonts();
+>>>>>>> Stashed changes
     MainWindow w;
     w.show();
     return a.exec();
