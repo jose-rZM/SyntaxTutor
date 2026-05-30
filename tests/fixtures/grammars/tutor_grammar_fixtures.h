0216a247510baeaf05dfda4d787c43eef2bc281c
jose-rZM <100773386+jose-rZM@users.noreply.github.com>
2026-05-30 17:23:05 +0200

    test: support list of fixtures

--- tests/fixtures/grammars/tutor_grammar_fixtures.h
+++ tests/fixtures/grammars/tutor_grammar_fixtures.h
@@ -2,11 +2,27 @@
 
 #include "grammar.hpp"
 
+#include <QList>
+#include <QString>
+
 namespace TutorGrammarFixtures {
+
+struct NamedGrammarFixture {
+    QString name;
+    Grammar grammar;
+};
 
 Grammar makeLl1SimpleGrammar();
 Grammar makeLl1EpsilonGrammar();
+Grammar makeLl1BranchingGrammar();
 Grammar makeSlrSimpleGrammar();
+Grammar makeSlrChainGrammar();
 Grammar makeSlrConflictGrammar();
+
+QList<NamedGrammarFixture> llFixtures();
+QList<NamedGrammarFixture> slrNoConflictFixtures();
+QList<NamedGrammarFixture> slrConflictFixtures();
+QList<NamedGrammarFixture> slrFixturesWithCbEpsilon();
+QList<NamedGrammarFixture> slrFixturesWithCbNonEpsilon();
 
 } // namespace TutorGrammarFixtures
